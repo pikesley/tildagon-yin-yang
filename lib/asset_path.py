5bfcac7fd84fbe543a89a7349107dbfd218673f1
Sam Pikesley <sam.pikesley@demandlogic.co.uk>
2026-04-06 22:16:36 +0100

    Fix `asset_path`

--- lib/asset_path.py
+++ lib/asset_path.py
@@ -5,8 +5,8 @@ apps = os.listdir("/apps")
 path = ""
 ASSET_PATH = "apps//"
 
-if "github_user_tildagon_yin_yang" in apps:
-    ASSET_PATH = "/apps/github_user_tildagon_yin_yang/"
+if "pikesley_tildagon_yin_yang" in apps:
+    ASSET_PATH = "/apps/pikesley_tildagon_yin_yang/"
 
 if "yin_yang" in apps:
     ASSET_PATH = "apps/yin_yang/"
